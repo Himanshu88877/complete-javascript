98eac94f3a22df2945e9d069f58709fc666bda2f
Himanshu88877 <146114835+Himanshu88877@users.noreply.github.com>
2023-12-14 20:47:59 +0530

    Add files via upload

---
+++ d2.js
@@ -0,0 +1,24 @@
+console.log("hello world")
+var a=5;
+console.log(a)
+var message,b;
+b=12;
+message ="hello world";
+console.log(message)
+console.log(b)
+
+
+// data type
+//number=1,2,3.. etc
+//string=text
+//boolen = true or false
+
+var x=5;//number type
+var z= "hello world";
+var y="hello";// string type
+console.log(y)
+
+var d=5;
+var f=5;
+console.log(d==f)//boolean type
+
---
+++ d3.js
@@ -0,0 +1,53 @@
+// keywords in js
+// 1. var
+// 2.let
+// 3.const
+
+// var a=10;
+// var b="hello world";
+// var c= null;
+
+
+// let d=6;
+// let num=5;
+
+// const cast=55;
+// const ele= 'hey...'
+
+// we can store any type of value in our keyword  this is called dynamic in nature.
+// hosting 
+
+
+// var kajal=23;//  global- scope
+// we can redeclare the var name again again in js 
+
+// let kajal=23; //block-scope
+// kajal=50;
+// we cannot redeclare the let name again again in js but resign kar sakte hai
+
+// const kajal=23;//block-scope
+// kajal =50; // never re-declare and resign the value in cont keyword
+// we cannot redeclare the const name again again in js and cannot resign . 
+
+
+// now value is 50 because js always excute line by line 
+// var kajal=50;
+// console.log(kajal)
+
+
+// let 
+//1 they can be declare in line and then intiallized with a value in another lines
+//2 they can be also declared and initalized in same line 
+// 3.they cannot be redeclared and 
+//but can be re-intialized.
+//4. we cannot have  two variable with same name in let keyword.
+let komal;
+komal=50;
+console.log(komal)
+
+//const
+//1.they can be declare in line and then intiallized with a value in same line only.
+// 2 they cannot be redeclared and but cannotbe re- initalized .
+// 3. we cannot have  two variable with same name in const keyword.
+
+
