5ea9af7d1238c07419884a5280b011d34d1180e4
HIMANSHU <146114835+Himanshu88877@users.noreply.github.com>
2024-01-15 10:36:55 +0530

    Add files via upload

---
+++ d1.js
@@ -0,0 +1,49 @@
+// introduction to javascript
+// created by BRENDEN EICH work in NETSCAPE  build js in 10 days.
+
+
+
+// two type of web pages 
+// 1.static web pages--user input not allow 
+// 2.dynamic web pages -- user input is allow and store (js is used to build dynamically web pages)
+
+// in 1990 
+// name os js 1. mocha 2.livescript 3.javascript( for mnarketing with java)
+//  java is compiler language and javascript is interpreter language 
+// java is interpreter scripting languages which is used to make our web pages dynamic 
+// ECMA(EUROPEAN COMPUTER MANUFACTURE ASSOCIATION) -- introduced by NETSCAPE maintain proper standarization of javascript
+
+// difference betweem compiler and interpreter 
+// Compiler languages
+// --take entire program at a time
+//  --compilation is done before execution
+//-- faster in working 
+// --required memory because it create object code
+// --display error in the end
+// --example-c++.c
+
+// interpreter languages 
+// --take single line of code at a time.
+// --compilation and execution occred simultaneously.
+// --slow in working.
+// --memmory is not required.
+// --display error after each line if exist.
+// --example-javascript,python
+
+
+// how compiler work ?
+// source code-->compile code--->machine code--->execute(output )
+ 
+// how interpreter work ?
+// source code-->compile code--->execute(output ) 
+
+// why js 
+// you can build web ,iot ,mobile App, using js 
+// js is used on frontend ,backend and also on databases like MongoDB 
+// js is dynamically in nature i.e object and array can be mixed types
+
+// js engine -to run js we need js engine 
+// ryan dahl wrote program in c++ and run using js engine  named nodejs which used on backend side
+// google chrome-- v8 engine
+// moxilla firefox-- super monkey engine
+// ie-- chakra
