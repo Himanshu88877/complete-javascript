a1308e1d9de8e7716558e733b59987faff433b21
HIMANSHU <146114835+Himanshu88877@users.noreply.github.com>
2024-01-15 11:48:01 +0530

    Add files via upload

---
+++ d6.js
@@ -0,0 +1,43 @@
+// increament and decreament 
+// a++ post inc 
+// ++a pre inc  already defined
+// a-- post dec 
+// --a pre dec  already defined
+
+
+// let a=5;
+// console.log(a++,a,a,a);//loop for futher inc.
+// 5,6,6,6
+// console.log(++a,a);
+// 7,7
+// console.log(a--,a);
+// 7,6
+// console.log(--a,a);
+// 5,5
+// this are also comma operator 
+
+
+// logical operators
+// &&(and),|| (or),!(not)
+// let b=5>10 && 2<5;
+// let b=5>4 && 2<5;
+// console.log(b)
+// let c=5>10 || 2<5;
+// console.log(c)
+// console.log(!true)
+// console.log(!false)
+
+// question 
+// let num1=3;
+// let num2=9;
+// console.log(num1+num2)
+// let a=3;
+// let b=1;
+// console.log(a<10 && a>b)
+
+
+// prompt 
+let age=prompt("enter your age") // by default datatype is strings
+console.log(age)
+
+
