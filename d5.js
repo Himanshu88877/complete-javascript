1ed6d8496cdc5f5ca3ac71818a3610efd0cbc3c9
HIMANSHU <146114835+Himanshu88877@users.noreply.github.com>
2024-01-15 11:42:00 +0530

    Add files via upload

---
+++ d5.js
@@ -0,0 +1,33 @@
+// comparison operator 
+let a=5;
+// let b=5;
+// let b='5';
+let b=6;
+console.log(a==b)  // to check both are same or not
+console.log(a===b)  // to check both are same or not also check datatype
+console.log(a=b)  // a assigned value b
+
+// confusion--guess there output
+console.log(5 != 5)
+// false
+console.log(5 != "5")
+// false
+console.log(5 != "6")
+// true
+console.log(5 !== "6")
+// true
+console.log(5 !== "5")
+// true
+console.log(5 !== 5)
+// false
+
+console.log(5>6)
+// false
+console.log(5<6)
+// true
+console.log(5<=6)
+// true
+console.log(5>=6)
+// false
+
+// console.log(10 %= 20)
