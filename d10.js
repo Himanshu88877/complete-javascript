98f794f8c75dc1116dcbd8a070435d3b790dcf3a
HIMANSHU <146114835+Himanshu88877@users.noreply.github.com>
2024-01-15 12:43:50 +0530

    Add files via upload

---
+++ d10.js
@@ -0,0 +1,32 @@
+// hosting in js
+a=10;
+console.log(a)
+// we use variable to store values 
+// var a;
+// in this case we get 10 because var is globally scoped
+
+// let a;
+// now we get error because let is blocked scope 
+
+// only var keyward is access in hosting at js
+// let or const are not 
+
+// export, extend variable used in Node.js
+
+// loop is used to perform repeated task 
+const n=5;
+for(let i=1;i<=n;i++){
+    console.log("i am programmer")
+
+}
+console.log("hi")
+for(let i=1;i<=n;++i){
+     console.log("i am programmer")
+}
+
+let num=prompt("enter a number",);
+console.log("table of number",num)
+for(let a=1;a<=10;a++){
+    console.log(num,"*",a,"=",num*a)
+}
+
