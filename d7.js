510a4dd8a9bdfaabb8c968e4eef554d296c659d4
HIMANSHU <146114835+Himanshu88877@users.noreply.github.com>
2024-01-15 12:07:42 +0530

    Add files via upload

---
+++ d7.js
@@ -0,0 +1,7 @@
+// conditional or ternary operator
+console.log((3===3)? "a" : "b")
+console.log((3===8)? "a" : "b",(3===3)? "a" : "b")
+// pass multiple condition
+
+console.log(7 === 8, 6 == 6? "green" : "red")
+// false,green 
