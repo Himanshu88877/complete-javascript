c9a262b16d88d85ff724937962c321613f04b957
HIMANSHU <146114835+Himanshu88877@users.noreply.github.com>
2024-01-15 11:35:19 +0530

    Add files via upload

---
+++ d4.js
@@ -0,0 +1,59 @@
+// let a=50;
+// console.log(a);
+// document.write(a);-- to write on html document
+// window.alert(a);--- alert dialog
+
+
+// not defined, undefined ,null
+// not defined= any variable which we try to acess but not declared will throw us a reference error; not defined 
+// console.log(myapp)
+
+
+// undefined - if a variable is declared but not assigned any value yet, but if we try to acess that variable then we get error as undefind
+
+let myhobbies;
+console.log(myhobbies)
+
+// null = it is default value assigned by the user to the variable as long as the variable has not been assigned .
+let amiadult=null;
+console.log(amiadult)
+amiadult=40;
+console.log(amiadult)
+
+// operators
+// 1 assignment operators
+let komal=34;
+// komal assign value 34 
+
+// 2 arithmetic operators
+// +,-,*,/,%.**
+let arun=3;
+let kunal=43;
+let aryan=arun+kunal;
+console.log(aryan)
+
+let num1=50;
+let num2=65;
+console.log(num1+num2)
+
+// subtractor 
+let n=50-10
+console.log(n)
+
+// multiplication 
+let x=2*4
+console.log(x)
+
+
+//divison
+let p=35/7
+console.log(p)
+
+//% give remminder
+let q=43%5
+console.log(q)
+// ** exponent 
+console.log(5**2)
+
+
+
