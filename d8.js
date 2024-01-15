3133991f3539d55e151c9bf082e30d372338b1b0
HIMANSHU <146114835+Himanshu88877@users.noreply.github.com>
2024-01-15 12:13:14 +0530

    Add files via upload

---
+++ d8.js
@@ -0,0 +1,55 @@
+// discison making 
+
+// let  x=5;
+// if (x>10){
+    // console.log( "x is graetor than 10");
+// }else{
+    // console.log( "x is less than 10 or equal to 10");
+
+// }
+
+// quetion on discison making 
+// questions
+// let a;
+// a= prompt("enter your input","")
+// if (a=="p" || a=="P"){
+//     console.log("Prepbytes")
+// }
+// else if (a=="Z" || a=="z"){
+//     console.log("zenith")
+// }
+// else if  (a=="E" || a=="e"){
+//     console.log("expert coder")
+// }
+// else if  (a=="D" || a=="d"){
+//     console.log("data structure")
+// }
+// else{
+//     console.log("try again")
+// }
+ 
+// question 3
+let a=parseInt(prompt("enter the value of a",""));
+let b=parseInt(prompt("enter the value of b",""));
+let c=parseInt(prompt("enter the value of c",""));
+if (a>b && a>c){
+    console.log("max num is "+ a)
+}
+else if(b>a && b>c){
+    console.log("max num is "+ b)
+}
+else if(c>a && c>b){
+    console.log("max num is "+ c)
+}
+else if(a==b && a==c){
+    console.log("your result is -1")
+
+}
+else{
+    console.log("try again")
+}
+
+
+//  parsetint-- it is used to take integer input 
+ 
+
