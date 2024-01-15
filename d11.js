ac3df8259b6590af28717d59bb337e67a43e5fec
HIMANSHU <146114835+Himanshu88877@users.noreply.github.com>
2024-01-15 13:12:12 +0530

    Add files via upload

---
+++ d11.js
@@ -0,0 +1,39 @@
+// for in loop --for object iteration
+/*
+const students={
+    name:"himanshu",
+    class:"js",
+    age:"20yrs"
+}
+for (let key in students){
+    console.log(`${key}=> ${students[key]}`)
+}
+
+const employee={
+    name:"Himanshu",
+    role:"fullstack developer",
+    salary:'60k'
+}
+for (let E in employee){
+    console.log(`${E}=> ${employee[E]}`)
+} */
+
+// while loop
+// let i=0;
+// while(i<10){
+//     text ="the number is"+ i;
+//     i++;
+//     console.log(text)
+
+// }
+// let x=0;
+// while(x<10){
+//     text ="the number is"+x;
+//     x++;
+//     console.log(text)
+
+// }
+
+for (let i=1;1<=10;i++){
+    console.log(3*i)
+}
