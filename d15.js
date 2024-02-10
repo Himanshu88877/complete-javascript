be15629c32e590903791e53c8c47a0381862f565
HIMANSHU <146114835+Himanshu88877@users.noreply.github.com>
2024-02-10 14:40:40 +0530

    Add files via upload

---
+++ d15.js
@@ -0,0 +1,52 @@
+// string concatenation 
+// let str1="i am a ";
+// let str2="developer";
+// let str3=str1+str2;
+
+// let str3=str1.concat(str2);
+// console.log(str3)
+
+//  slicing
+let name="you will be an amazing developer";
+// let newmessage = name.slice(7);
+// let newmessage = name.slice(7,13);
+let newmessage = name.slice(-17,-10);
+console.log(newmessage)
+// let str="javascript string methodes";
+// var res=str.slice(0,10);
+// console.log(res)
+
+// substring negative number is not allowed
+// unidirectional String slicing  
+// var str4="javascript string methods";
+// var res1 = str.substring(1,4);
+// console.log(res1)
+
+// replace 
+// var b="javascript methods string methods";
+// var result=b.replace("methods","tutorial");
+// var result=b.replaceAll("methods","tutorial");
+// var result=b.trim("methods");
+// var result=b.trim(" "); will discuss in array class
+// console.log(result)
+
+// split-- output array ki form me hota hai
+var a="1,2,3,4,5";
+var arr=a.split(",");
+console.log(arr)
+document.write(arr[0]) 
+document.write("<br>") 
+document.write(arr[1]) 
+
+
+// indexOf()
+// lastindexOf()
+// str.concat()
+// slice 
+// substring
+// replace
+// replaceall
+// split--output is in array form 
+
+
+
