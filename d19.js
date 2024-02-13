a8fc8e94bd95fc3469c405f0be5c87b8718fa1db
HIMANSHU <146114835+Himanshu88877@users.noreply.github.com>
2024-02-13 14:18:21 +0530

    Add files via upload

---
+++ d19.js
@@ -0,0 +1,73 @@
+//  method of array 
+// const arr=[1,4,9,16];
+// let a=arr.map(x=> x*2);
+// let a=arr.map(x=> x+2);
+// let a=arr.map(x=> x-2);
+// let a=arr.map(x=> x**2);
+// console.log(a)
+
+// pop --remove last value from array 
+const array=["jarvis","the","developer","if"];
+// console.log(array.pop())
+// console.log(array)
+
+// filter --
+
+// let a=array.filter(word=> word.length > 3);
+// let a=array.filter(word=> word.length <= 3);
+// let a=array.filter(word=> word.length < 3);
+// console.log(a)
+
+let newArr = array.concat(5,6,7)
+console.log(newArr)
+let newjoin = array.join(" ") // output is string
+console.log(newjoin)
+
+// let myarr=[1,2,3];
+// myarr.push(7)//add in end
+// myarr.pop()//remove from end
+// myarr.shift()//remove in starting
+// myarr.unshift("4","5")//add in starting
+// console.log(myarr)
+
+// let s=[4,5,6]
+// s.unshift(1)
+// s.unshift(2)
+// s.unshift(3)
+// console.log(s)
+
+let myar=["1","2","3","4","5","6","3","7"];
+
+// let a=myar.slice(1,3)
+// let a=myar.slice(1,3)
+// console.log(a)
+
+
+
+// let b=myar.indexOf("4")
+// console.log(b)
+
+// let c=myar.lastIndexOf("3")
+// let c=myar.lastIndexOf("7")
+// console.log(c)
+
+myar.reverse()
+console.log(myar)
+
+myar.sort()
+console.log(myar)
+
+// let month=["jan","march","april","june"];
+// month.splice(1,0,"feb")//first value is index value 2nd value is replace valu,3rd value is adding the new value
+// month.splice(2,2,"feb")//first value is index value 2nd value is replace valu,3rd value is adding the new value
+// month.splice(1,2,"feb")//first value is index value 2nd value is replace valu,3rd value is adding the new value
+// console.log(month)
+
+
+let ar1=[30,23,16,5,49,98,233];
+ar1.sort()
+console.log(ar1)
+
+// map,pop,filter,concat,join,shift,unshift,slice,indexOf.lastindexOf,
+//reverse,sort
+
