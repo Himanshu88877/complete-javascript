99313ffb0358b284875873f92e3dfc379db8e3db
HIMANSHU <146114835+Himanshu88877@users.noreply.github.com>
2024-02-13 13:58:18 +0530

    Add files via upload

---
+++ d17.js
@@ -0,0 +1,41 @@
+// let str="himanshu";
+// console.log(str.length)
+
+
+//  let a="hello world";
+//  let x= a.length
+//  console.log(x)
+
+// question on string 
+// 1.question
+// let inp="Hi Prepbytes";
+// let slicestr=inp.slice(3)
+// console.log(slicestr)
+
+// find the an element at index 11 in "Welcome to GeeksforGeeks"
+// let a="Welcome to GeeksforGeeks";
+// console.log(a[11])
+
+// Using replace() Method "   Welcome    to   Geeks    for   Geeks   "  Remove multiple spaces with single space
+// let a="   Welcome    to   Geeks    for   Geeks   " ;
+// let b=a.replaceAll("   "," ");
+// console.log(b);
+
+// function ext(a){
+//     console.log(a.slice(0,4))
+
+// }
+// ext("Robin Singh")
+
+// function letcount(a){
+
+//  s=a.split(",")
+//    console.log(s)
+// }
+// letcount("'The quick brown fox jumps over the lazy dog', 'the'")
+
+function letcount(a){
+ s=a.split(",")
+   console.log(s.length)
+}
+letcount("'the quick brown fox jumps over the lazy dog', 'the'")
