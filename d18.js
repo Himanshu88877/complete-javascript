3e8204ba7563837b8c5d9418b6ed155693680135
HIMANSHU <146114835+Himanshu88877@users.noreply.github.com>
2024-02-13 13:57:23 +0530

    Add files via upload

---
+++ d18.js
@@ -0,0 +1,25 @@
+//  array 
+ 
+//  let arr=[5,54,65];
+//  console.log(arr)
+
+//  let a=["hi","i am","himanshu"];
+//  console.log(a)
+
+//  method of array 
+// 1.push method 
+// it is used to add the  new value in end of array 
+const arr=["himanshu","the","developer"];
+// arr.push("developer");
+// console.log(arr)
+// let x=arr.slice(1,2)//slice an array
+// console.log(x)
+
+// let s=arr.toString()//convert array into string
+// console.log(s)
+
+arr.shift(); // remove first element from array and return that array
+console.log(arr)
+
+//push,slice,toString,shift
+
